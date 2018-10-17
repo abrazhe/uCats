cd6fa477edbb67904c67266960321e3cbd266486
Alexey Brazhe <brazhe@gmail.com>
2018-10-17 18:26:17 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ucats',
+      version = '0.0.1',
+      requires = ['numpy','scipy','image_funcut'],
+      py_modules=['μCats'],
+      scripts=['astrocats.py'],
+)
+
+
