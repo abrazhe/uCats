e3189975eb4a30880b249711e32097bc9e0be447
Alexey Brazhe <brazhe@gmail.com>
2018-10-17 18:38:18 +0300

    try enforcing unicode string

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='ucats',
       version = '0.0.1',
       requires = ['numpy','scipy','image_funcut'],
-      py_modules=['μCats'],
+      py_modules=[u'μCats'],
       scripts=['astrocats.py'],
 )
 
