dd4a0ab833b812e45dec26deec49c886fb4d9506
Alexey Brazhe <alexeybrazhe@yandex.ru>
2020-02-04 11:53:43 +0300

    add changes to __init__.py

--- ucats/__init__.py
+++ ucats/__init__.py
@@ -8,13 +8,17 @@ from . import baselines
 from . import events
 from . import detection1d
 from . import events
-from . import io_lif
+#from . import io_lif
 from . import masks
 from . import patches
 from . import scramble
 from . import utils
-from . import exponential_family
+from . import pmt
+
+from .pmt import  estimate_gain_and_offset
+from .anscombe import Anscombe
+from .utils import clip_outliers, mad_std
 
 
-impot numpy as np
+import numpy as np
 _dtype_ = np.float32
